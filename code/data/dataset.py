1ad71641198f12d615fc7b63074fa68c48e95a22
jamie-mcg <j.mcgowan.18@ucl.ac.uk>
2020-11-03 11:20:40 +0000

    Begun testing the data generator.

--- code/data/dataset.py
+++ code/data/dataset.py
@@ -6,31 +6,43 @@ from torch.utils.data import Dataset
 from linear_generator import LinearTask
 
 task_dict = {
-    "Linear": LinearTask
+    "linear": LinearTask
 }
 
 class TaskDataset(Dataset):
-    def __init__(self, parameter_args, task_args, transform=None):
+    def __init__(self, parameter_args, transform=None):
         self._transform = transform
         self._std_dev = parameter_args["std_dev"]
         self._num_tasks = parameter_args["num_tasks"]
+        self._task_type = parameter_args["task"].lower()
 
-        self._task_args = task_args
+        self.generate_parameters(parameter_args)
 
-        self.generate_parameters(parameters_args)
-
-    def generate_parameters(self, parameters):
+    def generate_parameters(self, parameter_args):
         self._parameters = []
-        for parameter in parameters_args["centers"]:
+        for parameter in parameter_args["centers"]:
             self._parameters.append(np.random.uniform(parameter, self._std_dev, self._num_tasks))
 
     def __len__(self):
         return len(self._num_tasks)
 
     def __getitem__(self, idx):
-        task = task_dict[self._task_type](args=self._parameters[:,idx], **self._task_args)
+        task = task_dict[self._task_type](a=self._parameters[0,idx], b=self._parameters[1, idx])
 
         return {
             "train": (task.x_train, task.y_train),
             "test": (task.x_test, task.y_test)
         }
+
+
+if __name__ == "__main__":
+    parameter_args = {
+        "task": "linear",
+        "centers": [2.0, 3.0],
+        "std_dev": 0.2,
+        "num_tasks": 1000
+    }
+
+    data = TaskDataset(parameter_args)
+
+    print(data._parameters)
